64d2b87bb34156e338d6d30737b9193077ccccc0
SyRos1 <yshinichiro3@gmail.com>
2022-01-02 03:21:00 +0900

    testing with econo

--- test git.R
+++ test git.R
@@ -1,2 +1,10 @@
 print("this file has been created in r studo")
 print("this file have been link to github")
+
+setwd("C:/Users/Asus/Documents/Econometric File R")
+data1= read.csv("injury.csv")
+attach(data1)
+
+#Question 1.A
+ols_q1A <- lm(ldurat ~ afchnge, data=subset(data1, ky==1 & highearn==1))
+summary(ols_q1A)
